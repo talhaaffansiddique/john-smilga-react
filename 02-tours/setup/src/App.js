5bbf593dd38511f4139e26bba531f9234346bf49
Talha Affan Siddique <talhaaffansiddique@gmail.com>
2023-04-18 13:04:05 +0500

    Revert "88 - Tours Fetch Data"
    This reverts commit 625eaf4e5d86f0ea8499c895143267fb9a80521c.
    
    # Conflicts:
    #	02-tours/setup/src/App.js

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
-import Loading from "./Loading";
-import Tours from "./Tours";
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = "https://course-api.com/react-tours-project";
+const url = 'https://course-api.com/react-tours-project'
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
@@ -40,4 +40,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
