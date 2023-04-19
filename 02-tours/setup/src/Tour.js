cf5cfd596aa333b82d12c3857a3d472fb3ce5fc8
Talha Affan Siddique <talhaaffansiddique@gmail.com>
2023-04-19 15:25:02 +0500

    90 - Tours Toggle Info

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,16 +1,27 @@
 import React, { useState } from "react";
 
 const Tour = ({ id, image, info, price, name }) => {
+  const [readMore, setReadMore] = useState(false);
+
   return (
-    <article>
+    <article className="single-tour">
       <img src={image} alt={name} />
       <footer>
         <div className="tour-info">
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
-        <p>{info}</p>
-        <button className="delete-btn">not intrested</button>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button
+            onClick={() => {
+              setReadMore(!readMore);
+            }}
+          >
+            {readMore ? "show less" : "read more"}
+          </button>
+        </p>
+        <button className="delete-btn">not intrested at all</button>
       </footer>
     </article>
   );
