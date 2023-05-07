cc7378db4e9b49442368ff84cbcf6d8cf67fccf9
talhaaffansiddique <talhaaffansiddique@gmail.com>
2023-05-07 10:49:20 +0500

    103 - Menu Categories Buttons dynamic approach

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
 
   const filterItems = (category) => {
     if (category === "all") {
@@ -21,9 +22,9 @@ function App() {
       <section className="menu section">
         <div className="title">
           <h2>our menu</h2>
-          <div className="underline"> </div>
+          <div className="underline"></div>
         </div>
-        <Categories filterItems={filterItems} />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
