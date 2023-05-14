9d4862a7f04222613431f3c60d00fe5f66e82bcd
talhaaffansiddique <talhaaffansiddique@gmail.com>
2023-05-14 10:46:14 +0500

    106 - Tabs Display First Job

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -26,8 +26,34 @@ function App() {
       </section>
     );
   }
+  const { company, dates, duties, title } = jobs[value];
 
-  return <h2>jobs</h2>;
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>experince</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="job-center">
+        {/* btn container  */}
+        {/* job info  */}
+
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default App;
