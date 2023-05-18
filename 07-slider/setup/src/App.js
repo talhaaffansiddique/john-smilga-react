0446261c7cd899010a26d56531865e4cf07daf5c
Talha Affan Siddique <talhaaffansiddique@gmail.com>
2023-05-18 18:16:48 +0500

    113 - Slider Autoplay

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -7,6 +7,25 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(1);
 
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
   return (
     <section className="section">
       <div className="title">
@@ -25,7 +44,7 @@ function App() {
           }
           if (
             personIndex === index - 1 ||
-            (index === 0 && personIndex === people.legth - 1)
+            (index === 0 && personIndex === people.length - 1)
           ) {
             position = "lastSlide";
           }
