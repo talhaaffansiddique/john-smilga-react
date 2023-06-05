e67d82c805f78bfc65a099f131d4a49cf3911acf
talhaaffansiddique <talhaaffansiddique@gmail.com>
2023-06-05 12:50:20 +0500

    121 - Color Generator Single Color

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -4,7 +4,7 @@ import SingleColor from "./SingleColor";
 import Values from "values.js";
 
 function App() {
-  const [color, setColor] = useState();
+  const [color, setColor] = useState("#f15025");
   const [error, setError] = useState(false);
   const [list, setList] = useState([]);
 
@@ -12,6 +12,7 @@ function App() {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10);
+      setList(colors);
 
       console.log(colors);
     } catch (error) {
@@ -38,7 +39,16 @@ function App() {
         </form>
       </section>
       <section className="colors">
-        <h4>List goes here</h4>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
       </section>
     </>
   );
