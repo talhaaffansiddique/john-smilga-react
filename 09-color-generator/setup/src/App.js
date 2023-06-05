ee9ea17a1ed3f3e7b91131176e22f82233896fa4
talhaaffansiddique <talhaaffansiddique@gmail.com>
2023-06-05 13:21:47 +0500

    123 - Color Generator Copy To Clipboard

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -4,9 +4,9 @@ import SingleColor from "./SingleColor";
 import Values from "values.js";
 
 function App() {
-  const [color, setColor] = useState("#f15025");
+  const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values("#f15025 "));
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -14,10 +14,10 @@ function App() {
       let colors = new Values(color).all(10);
       setList(colors);
 
-      console.log(colors);
+      // console.log(colors);
     } catch (error) {
       setError(true);
-      console.log(error);
+      // console.log(error);
     }
   };
 
