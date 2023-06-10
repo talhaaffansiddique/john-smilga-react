c6b725d44effde9b18639cbeef934e742c4248ad
talhaaffansiddique <talhaaffansiddique@gmail.com>
2023-06-10 19:56:49 +0500

    127 - Grocery Bud Alert

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -7,12 +7,18 @@ function App() {
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
-  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: "",
+    type: "",
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!name) {
       //display alert
+      // setAlert({ show: true, msg: "please enter value", type: "danger" });
+      showAlert(true, "danger", "please enter value");
     } else if (name && isEditing) {
       //deal with edit
     } else {
@@ -23,10 +29,14 @@ function App() {
     }
   };
 
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
+
   return (
     <section className="section-center">
       <form className="grocery-form " onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
         <h3>Grocery Bud</h3>
         <div className="form-control">
           <input
