37f1388a9ae353bf3a34d15edeb736f0caa9cdc2
Talha Affan Siddique <talhaaffansiddique@gmail.com>
2023-06-26 12:50:18 +0500

    129 - Grocery Bud Remove Item

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -39,15 +39,15 @@ function App() {
     showAlert(true, "danger", "empty list");
   };
 
-  const clearList = () => {
-    showAlert(true, "danger", "empty list");
-    setList([]);
+  const removeItem = (id) => {
+    showAlert(true, "danger", "item removed");
+    setList(list.filter((item) => item.id !== id));
   };
 
   return (
     <section className="section-center">
       <form className="grocery-form " onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
         <h3>Grocery Bud</h3>
         <div className="form-control">
           <input
@@ -64,7 +64,7 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className="grocery-container">
-          <List items={list} />
+          <List items={list} removeItem={removeItem} />
           <button className="clear-btn" onClick={clearList}>
             clear items
           </button>
