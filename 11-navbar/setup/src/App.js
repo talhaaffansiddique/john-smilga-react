7618c314333ea71f33ad7a258d7b5da518f9d150
Talha Affan Siddique <talhaaffansiddique@gmail.com>
2023-06-27 15:06:51 +0500

    135 - Navbar Simple Toggle

--- 11-navbar/setup/src/App.js
+++ 11-navbar/setup/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Navbar from "./Navbar";
-import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Sidebar />
     </>
   );
 }
