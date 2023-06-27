a294c36e2d32a6c3899bbf81381d6c10dfa1c82f
Talha Affan Siddique <talhaaffansiddique@gmail.com>
2023-06-27 15:26:31 +0500

    136 - Navbar Class Toggle

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -17,20 +17,23 @@ const Navbar = () => {
             <FaBars />
           </button>
         </div>
-        {showLinks && (
-          <div className="link-container show-container">
-            <ul className="links">
-              {links.map((link) => {
-                const { id, url, text } = link;
-                return (
-                  <li key={id}>
-                    <a href={url}>{text}</a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )}
+
+        <div
+          className={`${
+            showLinks ? "links-container show-container" : "links-container"
+          }`}
+        >
+          <ul className="links">
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
 
         <ul className="social-icons">
           {social.map((socialIcon) => {
