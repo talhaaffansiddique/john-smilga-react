ade82b2dc68d8a7499e11ec8c7e353d5e9a7983c
Talha Affan Siddique <talhaaffansiddique@gmail.com>
2023-08-01 17:31:27 +0500

    142 - SidebarModal Complete

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -3,14 +3,16 @@ import { FaBars } from "react-icons/fa";
 import { AppContext, useGlobalContext } from "./context";
 
 const Home = () => {
-  const data = useGlobalContext();
-  console.log(data);
+  const { openSidebar, openModal } = useGlobalContext();
+  //console.log(data);
   return (
     <main>
-      <button className="sidebar-toggle">
+      <button className="sidebar-toggle" onClick={openSidebar}>
         <FaBars />
       </button>
-      <button className="btn">Show Modal</button>{" "}
+      <button className="btn" onClick={openModal}>
+        Show Modal
+      </button>{" "}
     </main>
   );
 };
